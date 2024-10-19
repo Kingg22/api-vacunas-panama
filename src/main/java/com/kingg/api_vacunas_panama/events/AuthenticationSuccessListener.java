5c66148f065ff42212557a561cd8a0c8121ee5ba
Kingg <rey2213@outlook.com>
2024-10-19 10:28:36 -0500

    add log debug for authentication event. NOTE: when recreate bd need to FLUSHALL in Redis Cache

--- src/main/java/com/kingg/api_vacunas_panama/events/AuthenticationSuccessListener.java
+++ src/main/java/com/kingg/api_vacunas_panama/events/AuthenticationSuccessListener.java
@@ -2,12 +2,14 @@ package com.kingg.api_vacunas_panama.events;
 
 import com.kingg.api_vacunas_panama.service.UsuarioTransactionService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class AuthenticationSuccessListener implements ApplicationListener<AuthenticationSuccessEvent> {
@@ -15,9 +17,13 @@ public class AuthenticationSuccessListener implements ApplicationListener<Authen
 
     @Override
     public void onApplicationEvent(AuthenticationSuccessEvent event) {
+        log.debug("Authentication Success Event: {}", event.toString());
         if (event.getAuthentication() != null && event.getAuthentication().isAuthenticated()) {
             String userId = event.getAuthentication().getName();
-            usuarioTransactionService.updateLastUsed(UUID.fromString(userId));
+            log.debug("User ID for update last used: {}", userId);
+            if (userId != null && !userId.isBlank()) {
+                usuarioTransactionService.updateLastUsed(UUID.fromString(userId));
+            }
         }
     }
 
