098bd807c388a07f47c542f57d8c1cd3abdad3e1
Applepie2005 <allissonortega04@hotmail.com>
2024-10-19 11:47:13 -0500

    feat: add pdf base 64 endpoint
    add logo icon image to pdf template

--- src/main/java/com/kingg/api_vacunas_panama/service/PdfService.java
+++ src/main/java/com/kingg/api_vacunas_panama/service/PdfService.java
@@ -4,30 +4,46 @@ import com.itextpdf.html2pdf.HtmlConverter;
 import com.kingg.api_vacunas_panama.web.dto.DosisDto;
 import com.kingg.api_vacunas_panama.web.dto.PdfDto;
 import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
 
-import java.io.ByteArrayOutputStream;
+import java.io.*;
 import java.util.Base64;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @Service
+@RequiredArgsConstructor
 public class PdfService {
+    private final ResourceLoader resourceLoader;
+
+    private String getIconImageBase64() throws IOException {
+        Resource resource = resourceLoader.getResource("classpath:images/icon.png");
+        byte[] imageBytes = Files.readAllBytes(Paths.get(resource.getURI()));
+        return Base64.getEncoder().encodeToString(imageBytes);
+    }
 
     /**
      * Generate a PDF as a byte array
      */
-    public byte[] generatePdf(PdfDto pdfDto) {
+    //Al recibir una imagen de base64 se necesita usar IOException
+    public byte[] generatePdf(PdfDto pdfDto) throws IOException {
         String template = generateHtmlTemplate(pdfDto);
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         HtmlConverter.convertToPdf(template, outputStream);
         return outputStream.toByteArray();
     }
 
-    public String generatePdfBase64(PdfDto pdfDto) {
+    public String generatePdfBase64(PdfDto pdfDto) throws IOException {
         return Base64.getEncoder().encodeToString(generatePdf(pdfDto));
     }
 
-    private String generateHtmlTemplate(@NotNull PdfDto pdfDto) {
+    private String generateHtmlTemplate(@NotNull PdfDto pdfDto) throws IOException {
         StringBuilder dosisRows = new StringBuilder();
+        //Se convierte la imagen a base64 para remplazarla en el template
+        String base64Image = "data:image/jpeg;base64," + this.getIconImageBase64();
         String template = """
                 <!DOCTYPE html>
                 <html lang="es">
@@ -71,6 +87,12 @@ public class PdfService {
                             padding: 8px;
                             text-align: left;
                         }
+                        .logo {
+                                  position: absolute;
+                                  top: 20px;
+                                  right: 20px;
+                                  width: 150px; 
+                        }
                     </style>
                 </head>
                 <body>
@@ -80,6 +102,8 @@ public class PdfService {
                     <p>Esquema completo de vacunación</p>
                     <p>Complete vaccination scheme</p>
                 </div>
+                
+                <img src="{{base64Image}}" alt="Logo" class="logo">
                 
                 <div class="section">
                     <div class="subtitle">Apellidos y nombre:</div>
@@ -107,8 +131,9 @@ public class PdfService {
                 </div>
                 </body>
                 </html>
-                """;
-        template = template.replace("{{nombres}}", pdfDto.nombres())
+                """;// Insertar la imagen en Base64 en la plantilla HTML
+                template = template.replace("{{base64Image}}", base64Image);
+                template = template.replace("{{nombres}}", pdfDto.nombres())
                 .replace("{{apellidos}}", pdfDto.apellidos())
                 .replace("{{fecha_nacimiento}}", pdfDto.fechaNacimiento().toString());
 
--- src/main/java/com/kingg/api_vacunas_panama/web/controller/PruebasAlly.java
+++ src/main/java/com/kingg/api_vacunas_panama/web/controller/PruebasAlly.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
 
@@ -33,15 +34,16 @@ public class PruebasAlly {
     @GetMapping
     public ResponseEntity<byte[]> getPdfFile(@RequestParam("idVacuna") UUID idVacuna) {
         try {
-            // datos de PRUEBAS id Paciente = 4E40CB68-E567-4EBC-9669-75FAB2FDF933 (pasaporte MN876543C Tomas Gonzales)
-            // idVacuna = 7123E92C-EF0B-4001-8004-83578F932B4A (Hep A adultos)
+            // datos de PRUEBAS id Paciente = AF696D60-BC34-42BD-B525-084790EF6247
+            // idVacuna = 46EC208A-4719-4897-9E3A-CD3AC176EE2E (BCG)
             // para cambiar buscar en la base de datos docker los nuevos id
             // Todos estos pasos son de pruebas, yo me encargo de hacerlo funcionar después que el PDF se crea bien
-            List<DosisDto> dosisDtos = vacunaService.getDosisVacunaByIdPaciente(UUID.fromString("4E40CB68-E567-4EBC-9669-75FAB2FDF933"), idVacuna);
+            List<DosisDto> dosisDtos = vacunaService.getDosisVacunaByIdPaciente(UUID.fromString("AF696D60-BC34-42BD-B525-084790EF6247"), idVacuna);
             if (dosisDtos.isEmpty()) {
+                log.debug(dosisDtos.toString());
                 return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
             }
-            Paciente pDetalle = pacienteService.getPacienteById(UUID.fromString("4E40CB68-E567-4EBC-9669-75FAB2FDF933")).orElseThrow();
+            Paciente pDetalle = pacienteService.getPacienteById(UUID.fromString("AF696D60-BC34-42BD-B525-084790EF6247")).orElseThrow();
             String identificacion = pDetalle.getCedula() != null ? pDetalle.getCedula() : pDetalle.getPasaporte();
             String nombres = null;
             String apellidos = null;
@@ -66,33 +68,44 @@ public class PruebasAlly {
             return ResponseEntity.ok()
                     .headers(headers)
                     .body(pdfStream);
-        } catch (RuntimeException e) {
+        } catch (RuntimeException | IOException e) {
             log.debug(e.getMessage(), e);
             return ResponseEntity.internalServerError().build();
         }
     }
 
-/*    // después que funcione como archivo probar para de devolverlo como base64
-    // http://localhost:8080/vacunacion/v1/pdf/base64?idVacuna=....
     @GetMapping("/base64")
-    public ResponseEntity<IApiResponse<String, Serializable>> getPdfBase64(@RequestParam("idVacuna") UUID idVacuna, ServletWebRequest request) {
-        // uso de Rey para darle formato
-        IApiResponse<String, Serializable> apiResponse = new ApiResponse();
+    public ResponseEntity<String> getPdfBase64(@RequestParam("idVacuna") UUID idVacuna) {
         try {
-            List<DosisDto> dosisDtos = vacunaService.getDosisVacunaByIdPaciente(UUID.fromString("4E40CB68-E567-4EBC-9669-75FAB2FDF933"), idVacuna);
-            Paciente pDetalle = pacienteService.getPacienteById(UUID.fromString("4E40CB68-E567-4EBC-9669-75FAB2FDF933")).orElseThrow();
+            // datos de PRUEBAS id Paciente = AF696D60-BC34-42BD-B525-084790EF6247
+            // idVacuna =  46EC208A-4719-4897-9E3A-CD3AC176EE2E
+            // para cambiar buscar en la base de datos docker los nuevos id
+            // Todos estos pasos son de pruebas, yo me encargo de hacerlo funcionar después que el PDF se crea bien
+            List<DosisDto> dosisDtos = vacunaService.getDosisVacunaByIdPaciente(UUID.fromString("AF696D60-BC34-42BD-B525-084790EF6247"), idVacuna);
+            if (dosisDtos.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+            }
+            Paciente pDetalle = pacienteService.getPacienteById(UUID.fromString("AF696D60-BC34-42BD-B525-084790EF6247")).orElseThrow();
             String identificacion = pDetalle.getCedula() != null ? pDetalle.getCedula() : pDetalle.getPasaporte();
-            PdfDto pdfDto = new PdfDto(pDetalle.getNombre().concat(pDetalle.getNombre2()), pDetalle.getApellido1().concat(pDetalle.getNombre2()), identificacion, pDetalle.getFechaNacimiento().toLocalDate(), pDetalle.getId(), dosisDtos);
-            // utilizando el dto anterior haces el pdf
-            // Generar el PDF y devolverlo como base64
-            UUID idCertificado = UUID.randomUUID();
-            apiResponse.addData("pdf", pdfBase64);
-            apiResponse.addStatusCode(HttpStatus.OK);
-        } catch (RuntimeException runtimeException) {
-            log.error(runtimeException.getMessage(), runtimeException);
-            apiResponse.addStatusCode(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-        return ApiResponseUtil.sendResponse(apiResponse, request);
-    }*/
+            String nombres = null;
+            String apellidos = null;
+            // TODO hacer la concatenación de ambos datos si no es null
+            log.debug(dosisDtos.toString());
+            log.debug("Paciente ID: {}", pDetalle.getId());
+            log.debug("identificación a colocar: {}", identificacion);
+            PdfDto pdfDto = new PdfDto(pDetalle.getNombre(), pDetalle.getApellido1(), identificacion, pDetalle.getFechaNacimiento().toLocalDate(), pDetalle.getId(), dosisDtos);
+            log.debug(pdfDto.toString());
 
+            // Crear el DTO del PDF
+
+            // Utilizar el servicio para generar el PDF en Base64
+            String pdfBase64 = pdfService.generatePdfBase64(pdfDto);
+
+            // Devolver el PDF en Base64
+            return ResponseEntity.ok(pdfBase64);
+        } catch (RuntimeException | IOException e) {
+            log.debug(e.getMessage(), e);
+            return ResponseEntity.internalServerError().build();
+        }
+    }
 }
