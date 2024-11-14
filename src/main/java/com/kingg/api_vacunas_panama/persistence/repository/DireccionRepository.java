a3fe5b493149dd07ed8026ce5d2c172e57d6af56
Kingg22 <89825425+Kingg22@users.noreply.github.com>
2024-11-13 20:34:58 -0500

    fix: change to List when search a direccion
    add Hibernate initialize relations of Paciente -> Dirección -> Distrito -> Provincia

--- src/main/java/com/kingg/api_vacunas_panama/persistence/repository/DireccionRepository.java
+++ src/main/java/com/kingg/api_vacunas_panama/persistence/repository/DireccionRepository.java
@@ -3,15 +3,16 @@ package com.kingg.api_vacunas_panama.persistence.repository;
 import com.kingg.api_vacunas_panama.persistence.entity.Direccion;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface DireccionRepository extends JpaRepository<Direccion, UUID> {
 
-    Optional<Direccion> findDireccionByDireccionAndDistrito_Id(String direccion, int idDistrito);
+    Optional<List<Direccion>> findDireccionByDireccionAndDistrito_Id(String direccion, int idDistrito);
 
-    Optional<Direccion> findDireccionByDireccionContainingIgnoreCase(String direccion);
+    Optional<List<Direccion>> findDireccionByDireccionStartingWith(String direccion);
 
-    Optional<Direccion> findDireccionByDireccionAndDistrito_Nombre(String direccion, String distrito);
+    Optional<List<Direccion>> findDireccionByDireccionAndDistrito_Nombre(String direccion, String distrito);
 
 }
