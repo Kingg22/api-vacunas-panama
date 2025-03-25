aae3c3fc68bc263468161cf5c477c26cb4f37595
Rey <89825425+Kingg22@users.noreply.github.com>
2025-03-25 12:50:54 -0500

    test(Application): temporal patch of test suite

--- src/test/kotlin/io/github/kingg22/api_vacunas_panama/ApiVacunasPanamaApplicationTest.kt
+++ src/test/kotlin/io/github/kingg22/api_vacunas_panama/ApiVacunasPanamaApplicationTest.kt
@@ -1,9 +1,9 @@
 package io.github.kingg22.api_vacunas_panama
 
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 
-@SpringBootTest
+@DataJpaTest
 class ApiVacunasPanamaApplicationTests {
     @Test
     fun contextLoads() {}
