d8cba8ae5ed8fda5bbf7c6529c00846ea120dc29
Rey <89825425+Kingg22@users.noreply.github.com>
2025-03-25 19:30:15 -0500

    build(dependencies): add konvert (kotlin mapper) and spotless (linter, formatter)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,50 +1,78 @@
 plugins {
-	kotlin("jvm") version libs.versions.kotlin.get()
-	kotlin("plugin.spring") version libs.versions.kotlin.get()
-	kotlin("plugin.jpa") version libs.versions.kotlin.get()
+    kotlin("jvm") version libs.versions.kotlin.get()
+    kotlin("plugin.spring") version libs.versions.kotlin.get()
+    kotlin("plugin.jpa") version libs.versions.kotlin.get()
     java
-	alias(libs.plugins.spring.boot)
-	alias(libs.plugins.spring.dependency.management)
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.spring.dependency.management)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.spotless)
 }
 
 group = "io.github.kingg22"
 version = "0.2.0"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
 }
 
 configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
 }
 
 dependencies {
-	implementation(libs.bundles.implementation)
-	compileOnly(libs.lombok)
-	developmentOnly(libs.spring.boot.devtools)
-	runtimeOnly(libs.bundles.runtimeOnly)
-	annotationProcessor(libs.lombok)
-	annotationProcessor(libs.mapstruct.processor)
-	testImplementation(libs.bundles.testImplementation)
-	testRuntimeOnly(libs.junit.platform.launcher)
+    implementation(libs.bundles.implementation)
+
+    compileOnly(libs.lombok)
+
+    developmentOnly(libs.spring.boot.devtools)
+
+    runtimeOnly(libs.bundles.runtimeOnly)
+
+    annotationProcessor(libs.lombok)
+    annotationProcessor(libs.mapstruct.processor)
+    ksp(libs.konvert)
+
+    testImplementation(libs.bundles.testImplementation)
+
+    testRuntimeOnly(libs.junit.platform.launcher)
 }
 
 kotlin {
-	compilerOptions {
-		freeCompilerArgs.addAll("-Xjsr305=strict")
-	}
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
 }
 
 allOpen {
-	annotation("jakarta.persistence.Entity")
-	annotation("jakarta.persistence.MappedSuperclass")
-	annotation("jakarta.persistence.Embeddable")
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
+}
+
+spotless {
+    encoding("UTF-8")
+    java {
+        encoding("Cp1252")
+        palantirJavaFormat()
+        importOrder()
+        removeUnusedImports()
+    }
+    kotlin {
+        ktlint()
+    }
+    kotlinGradle {
+        ktlint()
+    }
+    sql {
+        target("src/main/resources/**/*.sql", "*.sql")
+    }
 }
