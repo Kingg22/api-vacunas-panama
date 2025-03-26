eb8929f5fad81e50fef891475a7eac9a14d4967d
Rey <89825425+Kingg22@users.noreply.github.com>
2025-03-26 17:59:53 -0500

    perf(repository): remove unused methods

--- src/main/java/io/github/kingg22/api/vacunas/panama/persistence/repository/SedeRepository.java
+++ src/main/java/io/github/kingg22/api/vacunas/panama/persistence/repository/SedeRepository.java
@@ -3,16 +3,14 @@ package io.github.kingg22.api.vacunas.panama.persistence.repository;
 import io.github.kingg22.api.vacunas.panama.persistence.entity.Sede;
 import io.github.kingg22.api.vacunas.panama.web.dto.UUIDNombreDto;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface SedeRepository extends JpaRepository<Sede, UUID> {
 
-    Optional<Sede> findByNombre(String nombre);
-
-    @Query(
-            "SELECT new io.github.kingg22.api.vacunas.panama.web.dto.UUIDNombreDto(s.id, s.nombre) FROM Sede s WHERE s.estado LIKE 'ACTIVO'")
+    @Query("SELECT new io.github.kingg22.api.vacunas.panama.web.dto.UUIDNombreDto(s.id, s.nombre) "
+            + "FROM Sede s "
+            + "WHERE s.estado LIKE 'ACTIVO'")
     List<UUIDNombreDto> findAllIdAndNombre();
 }
