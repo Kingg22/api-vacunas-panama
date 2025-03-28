7db4b0c8b5144bd6243e145fe6d8963611f30c7b
Rey <89825425+Kingg22@users.noreply.github.com>
2025-03-27 23:45:26 -0500

    perf(services): use interface instead of implementation

--- src/main/java/io/github/kingg22/api/vacunas/panama/service/ISedeService.java
+++ src/main/java/io/github/kingg22/api/vacunas/panama/service/ISedeService.java
@@ -1,8 +1,13 @@
 package io.github.kingg22.api.vacunas.panama.service;
 
+import io.github.kingg22.api.vacunas.panama.persistence.entity.Sede;
 import io.github.kingg22.api.vacunas.panama.web.dto.UUIDNombreDto;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface ISedeService {
     List<UUIDNombreDto> getIdNombreSedes();
+
+    Optional<Sede> getSedeById(UUID id);
 }
