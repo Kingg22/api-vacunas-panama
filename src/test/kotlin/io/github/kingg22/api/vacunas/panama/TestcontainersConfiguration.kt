b063030de838261883249f4906044ecad4a26f0c
Rey <89825425+Kingg22@users.noreply.github.com>
2025-04-11 20:07:18 -0500

    test(testcontainers): update to use testcontainers and add default files
    maybe need to update configuration of test

---
+++ src/test/kotlin/io/github/kingg22/api/vacunas/panama/TestcontainersConfiguration.kt
@@ -0,0 +1,7 @@
+package io.github.kingg22.api.vacunas.panama
+
+import io.github.kingg22.api.vacunas.panama.util.BaseIntegrationTest
+import org.springframework.test.context.ActiveProfiles
+
+@ActiveProfiles("test")
+class TestcontainersConfiguration : BaseIntegrationTest()
