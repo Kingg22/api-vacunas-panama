ed47743ba5e4ddba9d842de8da26db3eb1d5182a
Rey <89825425+Kingg22@users.noreply.github.com>
2025-04-13 16:21:05 -0500

    perf(doctor): remove unused method

--- src/main/kotlin/io/github/kingg22/api/vacunas/panama/modules/doctor/service/DoctorService.kt
+++ src/main/kotlin/io/github/kingg22/api/vacunas/panama/modules/doctor/service/DoctorService.kt
@@ -1,25 +1,17 @@
 package io.github.kingg22.api.vacunas.panama.modules.doctor.service
 
-import io.github.kingg22.api.vacunas.panama.modules.doctor.entity.Doctor
+import io.github.kingg22.api.vacunas.panama.modules.doctor.dto.DoctorDto
 import java.util.Optional
 import java.util.UUID
 
 /** Service interface for managing and retrieving doctor-related information. */
-interface DoctorService {
+fun interface DoctorService {
 
     /**
-     * Retrieves a [Doctor] entity associated with the specified user ID.
-     *
-     * @param idUser UUID of the user linked to the doctor.
-     * @return [Optional] containing the doctor if found, or empty if not.
-     */
-    fun getDoctorByUserID(idUser: UUID): Optional<Doctor>
-
-    /**
-     * Retrieves a [Doctor] entity by its unique doctor ID.
+     * Retrieves a [DoctorDto] by its unique doctor ID.
      *
      * @param idDoctor UUID of the doctor to retrieve.
      * @return [Optional] containing the doctor if found, or empty if not.
      */
-    fun getDoctorById(idDoctor: UUID): Optional<Doctor>
+    fun getDoctorById(idDoctor: UUID): Optional<DoctorDto>
 }
