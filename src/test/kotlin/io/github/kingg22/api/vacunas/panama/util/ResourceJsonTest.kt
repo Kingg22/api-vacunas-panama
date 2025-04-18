a6a14bb0139db559b420dbb1bcfde202eb25ebea
Rey <89825425+Kingg22@users.noreply.github.com>
2025-04-17 21:41:09 -0500

    test(token controller): add an integration test to refresh the token with a real access token

--- src/test/kotlin/io/github/kingg22/api/vacunas/panama/util/ResourceJsonTest.kt
+++ src/test/kotlin/io/github/kingg22/api/vacunas/panama/util/ResourceJsonTest.kt
@@ -7,3 +7,13 @@ fun retrieveFileJson(path: String) = ClassPathResource(path).inputStream.buffere
 
 /** Remove all metadata with not testable information */
 fun String.removeMetadata() = this.replace(",\\s*\"timestamp\":\\s*\".*?\"".toRegex(), "")
+
+fun String.extractJsonToken(path: String): String {
+    val json = com.fasterxml.jackson.module.kotlin.jacksonObjectMapper().readTree(this)
+    val keys = path.removePrefix("$.").split(".")
+    var node = json
+    for (key in keys) {
+        node = node[key]
+    }
+    return node.asText()
+}
