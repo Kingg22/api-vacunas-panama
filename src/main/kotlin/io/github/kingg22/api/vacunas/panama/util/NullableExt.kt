db101c82a183facbae2f799f272787614e7904e9
Rey <89825425+Kingg22@users.noreply.github.com>
2025-04-18 11:48:32 -0500

    perf(compiler): add contract dsl to nullable extension

--- src/main/kotlin/io/github/kingg22/api/vacunas/panama/util/NullableExt.kt
+++ src/main/kotlin/io/github/kingg22/api/vacunas/panama/util/NullableExt.kt
@@ -1,6 +1,9 @@
 package io.github.kingg22.api.vacunas.panama.util
 
 import reactor.core.publisher.Mono
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
 
 /**
  * Returns the receiver object if it is not null; otherwise, returns the provided alternative.
@@ -8,7 +11,13 @@ import reactor.core.publisher.Mono
  * @param other The alternative value to return if the receiver object is null.
  * @return The receiver object if it is not null, otherwise the alternative value.
  */
-infix fun <T> T?.or(other: T?) = this ?: other
+@OptIn(ExperimentalContracts::class)
+infix fun <T> T?.or(other: T?): T? {
+    contract {
+        returnsNotNull() implies (this@or != null || other != null)
+    }
+    return this ?: other
+}
 
 /**
  * Returns the receiver object if it is not null; otherwise, returns the provided alternative.
@@ -16,7 +25,14 @@ infix fun <T> T?.or(other: T?) = this ?: other
  * @param other The alternative value to return if the receiver object is null.
  * @return The receiver object if it is not null, otherwise the alternative value.
  */
-infix fun <T> T?.or(other: () -> T?) = this ?: other()
+@OptIn(ExperimentalContracts::class)
+infix fun <T> T?.or(other: () -> T?): T? {
+    contract {
+        returnsNotNull() implies (this@or != null)
+        callsInPlace(other, InvocationKind.AT_MOST_ONCE)
+    }
+    return this ?: other()
+}
 
 /**
  * Returns the receiver if it is not null; otherwise, returns the specified alternative value.
@@ -32,7 +48,13 @@ infix fun <T> T?.orElse(other: T) = this ?: other
  * @param other The alternative value to return if the receiver is null.
  * @return The receiver if not null, otherwise the specified alternative value _non null_.
  */
-infix fun <T> T?.orElse(other: () -> T) = this ?: other()
+@OptIn(ExperimentalContracts::class)
+infix fun <T> T?.orElse(other: () -> T): T {
+    contract {
+        callsInPlace(other, InvocationKind.AT_MOST_ONCE)
+    }
+    return this ?: other()
+}
 
 /**
  * Executes the given `presentConsumer` function if the nullable receiver is not null.
@@ -41,7 +63,12 @@ infix fun <T> T?.orElse(other: () -> T) = this ?: other()
  * @param presentConsumer a function to be executed if the receiver is not null.
  * @param missingAction a function to be executed if the receiver is null.
  */
+@OptIn(ExperimentalContracts::class)
 fun <T> T?.ifPresentOrElse(presentConsumer: (T) -> Unit, missingAction: () -> Unit) {
+    contract {
+        callsInPlace(presentConsumer, InvocationKind.AT_MOST_ONCE)
+        callsInPlace(missingAction, InvocationKind.AT_MOST_ONCE)
+    }
     if (this != null) presentConsumer(this) else missingAction()
 }
 
