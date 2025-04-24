765c40003abca4553630e59efc7dc8023cf6b550
Rey <89825425+Kingg22@users.noreply.github.com>
2025-04-24 18:32:33 -0500

    fix(dns java): add temporal patch, to test in render

--- src/main/kotlin/io/github/kingg22/api/vacunas/panama/ApiVacunasPanamaApplication.kt
+++ src/main/kotlin/io/github/kingg22/api/vacunas/panama/ApiVacunasPanamaApplication.kt
@@ -2,12 +2,17 @@ package io.github.kingg22.api.vacunas.panama
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.security.Security
+import java.time.ZoneOffset.UTC
 import java.util.TimeZone
 
 @SpringBootApplication
 class ApiVacunasPanamaApplication
 
 fun main(args: Array<String>) {
-    TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
+    TimeZone.setDefault(TimeZone.getTimeZone(UTC))
+    // Temporal fix to: WARNING:
+    // Unable to disable JVM DNS caching disabled in favor of link-local DNS caching because /layers/paketo-buildpacks_bellsoft-liberica/java-security-properties/java-security.properties is read-only
+    Security.setProperty("networkaddress.cache.ttl", "0")
     runApplication<ApiVacunasPanamaApplication>(*args)
 }
