60c261eb902b1d43a28b301d2f27664db8a8e352
Rey <89825425+Kingg22@users.noreply.github.com>
2025-04-28 20:12:39 -0500

    test(containers): add @Rule to avoid problems with an uncleaned database

--- src/test/kotlin/io/github/kingg22/api/vacunas/panama/TestcontainersConfiguration.kt
+++ src/test/kotlin/io/github/kingg22/api/vacunas/panama/TestcontainersConfiguration.kt
@@ -1,17 +1,23 @@
 package io.github.kingg22.api.vacunas.panama
 
+import org.junit.Rule
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection
 import org.springframework.context.annotation.Bean
 import org.springframework.test.context.ActiveProfiles
 import org.testcontainers.containers.GenericContainer
+import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.utility.DockerImageName
 
 @TestConfiguration(proxyBeanMethods = false)
 @ActiveProfiles("test")
 class TestcontainersConfiguration {
     @Bean
+    @Rule
     @ServiceConnection(name = "redis")
     fun redisContainer(): GenericContainer<*> =
         GenericContainer(DockerImageName.parse("redis:latest")).withExposedPorts(6379)
+
+    @Rule
+    fun postgresContainer(): PostgreSQLContainer<*> = PostgreSQLContainer("postgres:17-alpine")
 }
