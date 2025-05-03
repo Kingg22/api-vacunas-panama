acd796cb8a283911e54fcae06b7bdb367ca25baa
Rey <89825425+Kingg22@users.noreply.github.com>
2025-05-02 23:01:42 -0500

    fix(public controller): in jakarta o quarkus doesn't need a base path to redirect

--- src/main/kotlin/io/github/kingg22/api/vacunas/panama/controller/PublicResourcesController.kt
+++ src/main/kotlin/io/github/kingg22/api/vacunas/panama/controller/PublicResourcesController.kt
@@ -14,29 +14,29 @@ class PublicResourcesController {
 
     @GET
     @Path("/distritos")
-    suspend fun getDistritos() = permanentRedirect("/vacunacion/v1/direccion/distritos")
+    suspend fun getDistritos() = permanentRedirect("/direccion/distritos")
 
     @GET
     @Path("/provincias")
-    suspend fun getProvincias() = permanentRedirect("/vacunacion/v1/direccion/provincias")
+    suspend fun getProvincias() = permanentRedirect("/direccion/provincias")
 
     @GET
     @Path("/sedes")
-    suspend fun getSedes() = permanentRedirect("/vacunacion/v1/sedes")
+    suspend fun getSedes() = permanentRedirect("/sedes")
 
     @GET
     @Path("/vacunas")
-    suspend fun getVacunas() = permanentRedirect("/vacunacion/v1/vaccines")
+    suspend fun getVacunas() = permanentRedirect("/vaccines")
 
     @GET
     @Path("/vaccines")
-    suspend fun getVaccines() = permanentRedirect("/vacunacion/v1/vaccines".toUri())
+    suspend fun getVaccines() = permanentRedirect("/vaccines".toUri())
 
     @GET
     @Path("/roles")
-    suspend fun getRoles() = permanentRedirect("/vacunacion/v1/roles".toUri())
+    suspend fun getRoles() = permanentRedirect("/roles".toUri())
 
     @GET
     @Path("/roles/permisos")
-    suspend fun getPermisos() = permanentRedirect("/vacunacion/v1/roles/permisos".toUri())
+    suspend fun getPermisos() = permanentRedirect("/roles/permisos".toUri())
 }
