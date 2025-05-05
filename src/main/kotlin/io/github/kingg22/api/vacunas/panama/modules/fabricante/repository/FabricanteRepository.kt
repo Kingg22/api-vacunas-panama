a8430ee903f94d5423145621a0f912d528855411
Rey <89825425+Kingg22@users.noreply.github.com>
2025-05-05 14:41:33 -0500

    fix(repository): add missing `with session` in FabricanteRepository

--- src/main/kotlin/io/github/kingg22/api/vacunas/panama/modules/fabricante/repository/FabricanteRepository.kt
+++ src/main/kotlin/io/github/kingg22/api/vacunas/panama/modules/fabricante/repository/FabricanteRepository.kt
@@ -1,6 +1,7 @@
 package io.github.kingg22.api.vacunas.panama.modules.fabricante.repository
 
 import io.github.kingg22.api.vacunas.panama.modules.fabricante.entity.Fabricante
+import io.github.kingg22.api.vacunas.panama.util.withSession
 import io.quarkus.hibernate.reactive.panache.kotlin.PanacheRepositoryBase
 import io.smallrye.mutiny.coroutines.awaitSuspending
 import jakarta.enterprise.context.ApplicationScoped
@@ -8,11 +9,13 @@ import java.util.UUID
 
 @ApplicationScoped
 class FabricanteRepository : PanacheRepositoryBase<Fabricante, UUID> {
-    suspend fun findByIdOrNull(id: UUID): Fabricante? = findById(id).awaitSuspending()
+    suspend fun findByIdOrNull(id: UUID): Fabricante? = withSession { findById(id).awaitSuspending() }
 
-    suspend fun findByLicencia(licencia: String): Fabricante? =
+    suspend fun findByLicencia(licencia: String): Fabricante? = withSession {
         find("licencia = ?1", licencia).firstResult().awaitSuspending()
+    }
 
-    suspend fun findByUsuarioId(idUsuario: UUID): Fabricante? =
+    suspend fun findByUsuarioId(idUsuario: UUID): Fabricante? = withSession {
         find("usuario.id = ?1", idUsuario).firstResult().awaitSuspending()
+    }
 }
